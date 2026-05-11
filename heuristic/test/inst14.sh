f50db322138aee4c6d39f9f837c57bbee98066c9
GabrieleRamagnano <119187111+GabrieleRamagnano@users.noreply.github.com>
2026-05-11 09:12:26 +0200

    pddl_70 plot

--- heuristic/test/inst14.sh
+++ heuristic/test/inst14.sh
@@ -4,7 +4,7 @@ declare pkg="./packgs0.csv"
 declare inst="./inst13.sh"
 
 tail -n +2 "${pkg}" | while IFS=',' read -r name package label _tail _runtl ;do
-    if [[ "${name}" == "PDDL_Clingcon105" ]]; then
+    if [[ "${name}" == "PDDL_TEST70" ]]; then
 	echo "${name}"
 	  { export tag="${name}" 
 	    bash "${inst}" move
@@ -15,7 +15,7 @@ done
 
 
 tail -n +2 "${pkg}" | while IFS=',' read -r name package label _tail _runtl ;do
-    if [[ "${name}" == "PDDL_DHXplink105" ]]; then
+    if [[ "${name}" == "-----" ]]; then
        echo "${name}"
 	  { export tag="${name}" 
 	    bash "${inst}" move
