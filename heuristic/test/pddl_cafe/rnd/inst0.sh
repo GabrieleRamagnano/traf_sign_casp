e2458e1991197f418f70e3e66b814677ad88a6ce
GabrieleRamagnano <119187111+GabrieleRamagnano@users.noreply.github.com>
2026-05-12 15:21:10 +0200

    pddl-cafe and pddl-minus tests set

---
+++ heuristic/test/pddl_cafe/rnd/inst0.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+horizon_s=("600" "660" "720" "780" "840" "900")
+random="./../result/Instancesv2_round/random"
+heulink="combine_pddl_dhxplink.sh"
+
+function exe_heulink
+{
+    for hrz in "${horizon_s[@]}"; do
+        bash "${heulink}" "${random}" "${hrz}" 
+    done
+}
+
+function execute
+{
+    exe_heulink &
+}
+
+"$@"
