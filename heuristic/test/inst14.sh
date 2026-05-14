a8435b4d8ec62181344fc9ae17c94187d8a9372a
alice <alice.tarzariol@aau.at>
2026-05-14 11:24:01 +0000

    pddl-clingcon test ended

--- heuristic/test/inst14.sh
+++ heuristic/test/inst14.sh
@@ -4,7 +4,7 @@ declare pkg="./packgs0.csv"
 declare inst="./inst13.sh"
 
 tail -n +2 "${pkg}" | while IFS=',' read -r name package label _tail _runtl ;do
-    if [[ "${name}" == "PDDL_TEST70" ]]; then
+    if [[ "${name}" == "PDDL_DHXplinkComb" ]]; then
 	echo "${name}"
 	  { export tag="${name}" 
 	    bash "${inst}" move
